db5f0eef920a67593b79f80680de4cb607a71c57
Abraham Aviles <aaaviles1995@gmail.com>
2021-01-13 14:19:24 -0500

    commit tree fb2ab5d032a5050eab905f4abb2d77ba2c08ef68

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,29 @@
 require 'pry'
 
 class String
-
-  def sentence?
     
+  def sentence?
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
 
   def count_sentences
-
+      x = 0
+      ans = 0
+      arr = self.split
+      while arr.length > x do
+        if arr[x].sentence? || arr[x].question? || arr[x].exclamation?
+          ans += 1
+        end
+        x += 1
+      end
+      return ans
   end
 end
